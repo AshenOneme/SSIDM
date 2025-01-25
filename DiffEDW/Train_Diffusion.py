57584a706d46878abd923c1d7542d173e118ee81
Tianyang Zhang <98397090+AshenOneme@users.noreply.github.com>
2025-01-25 11:44:44 +0800

    Update Train_Diffusion.py

--- DiffEDW/Train_Diffusion.py
+++ DiffEDW/Train_Diffusion.py
@@ -20,33 +20,28 @@ train_loader = torch.utils.data.DataLoader(dataset=diffusiondataset,batch_size=1
 
 device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-# 创建一个调度器
 noise_scheduler = DDPMScheduler(num_train_timesteps=1000, beta_schedule='squaredcos_cap_v2')
 # noise_scheduler = DDIMScheduler(num_train_timesteps=50)
 
 n_epochs = 100
 loss_fn = nn.MSELoss()
 net = ClassConditionedUnet().to(device)
-# model_state_dict = torch.load('./model_pretrain.pt')
-# net.load_state_dict(model_state_dict.state_dict())
-# Define optimizer
 optimizer = optim.AdamW(net.parameters(), lr=1e-3)
 # Define learning rate scheduler
 scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.95)
 
-# 训练开始
+
 for epoch in range(n_epochs):
     with tqdm(train_loader) as pbar:
         for i, (x, y) in enumerate(pbar):
-            # 获取数据并添加噪声
-            x = x.to(device)*2-1  # 数据被归一化到区间(-1, 1)
+            x = x.to(device)*2-1
             y = y.to(device)
 
             noise = torch.randn_like(x)
             timesteps = torch.randint(0, 999, (x.shape[0],)).long().to(device)
             noisy_x = noise_scheduler.add_noise(x, noise, timesteps)
-            pred = net(noisy_x, timesteps,y)  # 注意这里输入了类别信息
-            # 计算损失值
+            pred = net(noisy_x, timesteps,y)
+
             loss = loss_fn(pred, noise)
             optimizer.zero_grad()
             loss.backward()
